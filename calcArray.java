3e824156d57b3cc3f709d96dba387bc5fc4e8563
marcuschisholm <35776241+marcuschisholm@users.noreply.github.com>
2018-04-05 13:23:15 -0400

    Add files via upload

--- calcArray.java
+++ calcArray.java
@@ -1,5 +1,19 @@
 package calc;
 
-public class calcArray {
+import java.util.Scanner;
 
+public class calcArray {
+	public static void main (String[]args){
+		
+	
+	Scanner scan=new Scanner (System.in);
+	int[]numbers={12,2,32,45,11};
+	
+	
+	int sum=0;
+	for(int i=0;i<numbers.length;i++)
+		sum= sum+ numbers[i];
+	double avarage= sum/ numbers.length;
+			System.out.println("avarage value of array is "+ avarage);
+}
 }
--- grid.java
+++ grid.java
@@ -1,5 +1,18 @@
 package calc;
 
-public class grid {
+import java.util.Scanner;
 
+public class grid{ 
+	public static void main (String[]args){
+		String[]x={"x","x","x","x","x"};
+				for(int y=0;y<5;y++){
+					System.out.println();
+					for(int z=0;z<5;z++){
+						System.out.println(x[y]);
+					}
+				}
+	}
 }
+
+				
+		
